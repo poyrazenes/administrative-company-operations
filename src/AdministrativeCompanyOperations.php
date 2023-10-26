bf22f62a7b982ea741f8af191beda058cbeeae0a
Enes Poyraz <enes.poyraz@4alabs.io>
2023-10-26 14:02:34 +0300

    update

---
+++ src/AdministrativeCompanyOperations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Poyrazenes\AdministrativeCompanyOperations;
+
+use Illuminate\Support\Facades\Http;
+
+class AdministrativeCompanyOperations
+{
+    public function justDoIt()
+    {
+        return Http::get('https://littlelunches.com/en/home');
+    }
+}
---
+++ src/Providers/AdministrativeCompanyOperationsServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Poyrazenes\AdministrativeCompanyOperations\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdministrativeCompanyOperationsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
