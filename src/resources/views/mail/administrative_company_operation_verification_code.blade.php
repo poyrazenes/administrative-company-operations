d6f508ce0879a61593ddc5a37f253ddd6eaeaff0
poyrazenes <enes.poyraz@4alabs.io>
2023-11-01 10:41:41 +0300

    update

--- src/resources/views/mail/administrative_company_operation_verification_code.blade.php
+++ src/resources/views/mail/administrative_company_operation_verification_code.blade.php
@@ -2,6 +2,9 @@
 <html>
 <body>
 Code to verification: <h1>{{ $code }}</h1>
-<p>Thank you</p>
+<br><br>
+Project: <strong>{{ config('app.name') }}</strong>
+<br><br>
+Environment: <strong>{{ app()->environment() }}</strong>
 </body>
 </html>
